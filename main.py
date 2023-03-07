4eda91e3ec2183f27d6a5dfbff1c7396fde2ad95
Chloe <laichloe@ufl.edu>
2023-03-07 12:59:48 -0500

    Added menu options and its if-statements, and defined function for encoder

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-print("Menu")
-print("-------------")
-print("1. Encode\n2. Decode\n3. Quiz\n")
-option = int(input("Please enter an option: "))
+def menu_options():
+    print("Menu")
+    print("-------------")
+    print("1. Encode\n2. Decode\n3. Quiz\n")
+
+
+def encoder(password):
+    encoded = ""
+    for num in password:
+        number = int(num) + 3
+        encoded += str(number)
+    return encoded
+
+
+def decoder(encoded_num):
+    pass
+
+
+if __name__ == "__main__":
+    menu_options()
+    option = int(input("Please enter an option: "))
+
+    while True:
+        if option == 1:
+            password = input("Please enter your password to encode: ")
+            encoded_num = encoder(password)
+            print("Your password has been encoded and stored!")
+        if option == 2:
+            pass
+        if option == 3:
+            break
